d802de152ef17bff1e5f8d969a4c6acde03ec000
subekshyakharel <subekshyakharel922@gmail.com>
2025-07-28 16:57:30 +0545

    viteConfig added

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,5 +9,12 @@ export default defineConfig({
       "@": Path.resolve(__dirname, "./src"),
       "@components": Path.resolve(__dirname, "./src/components"),
     },
+  },
+  // Add this to enable SPA fallback for dev server
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: "dist", // default, can be omitted if you like
   },
 });
